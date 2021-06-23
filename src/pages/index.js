88c407e68e72d923f8570100084d2c74138b544b
Jake Robertson <jake@faltro.com>
2021-06-22 18:37:18 -0700

    remove duplicated title from home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
+import React from "react";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./index.module.css";
+import HomepageFeatures from "../components/HomepageFeatures";
 
 function HomepageHeader() {
   return (
@@ -12,22 +11,19 @@ function HomepageHeader() {
       <div className={styles.heroInner}>
         <h1 className={styles.heroProjectTagline}>
           <img
-            alt={'Houdoku logo'}
+            alt={"Houdoku logo"}
             className={styles.heroLogo}
-            src={useBaseUrl('/img/logo.svg')}
+            src={useBaseUrl("/img/logo.svg")}
           />
-          <span
-            className={styles.heroTitleTextHtml}
-
-          >Houdoku is a <b>free</b> and open source <b>manga reader</b>.</span>
+          <span className={styles.heroTitleTextHtml}>
+            Houdoku is a <b>free</b> and open source <b>manga reader</b>.
+          </span>
         </h1>
         <div className={styles.indexCtas}>
           <Link className="button button--primary" to="/download">
             <span>Download</span>
           </Link>
-          <Link
-            className="button button--info"
-            to="/docs/intro">
+          <Link className="button button--info" to="/docs/intro">
             <span>View Docs</span>
           </Link>
         </div>
@@ -37,11 +33,8 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="Houdoku is a free and open source manga reader for the desktop">
+    <Layout description="Houdoku is a free and open source manga reader for the desktop">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
