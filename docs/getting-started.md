99498614341e67f471f05500bf59a491cfe8bc0b
Jake Robertson <jake@faltro.com>
2021-08-29 13:23:18 -0700

    revise getting started page

--- docs/getting-started.md
+++ docs/getting-started.md
@@ -1,42 +1,14 @@
 ---
 sidebar_position: 0
+slug: /
 ---
 
 # Getting Started
 
-## Installation
-
 Download the latest version of Houdoku for your OS from the [download page](/download).
 
-If you get a warning while installing, see the [Windows SmartScreen Warning page](/docs/known-issues/windows-smartscreen-warning).
+```mdx-code-block
+import {GettingStartedRow} from '@site/src/components/GettingStarted';
 
-## (optional) Install Extensions
-
-If you want to read manga from an aggregator/scanlator website, you'll need to install an extension.
-
-Go to the `Extensions` tab on the left side of the client. Search for an extension you want to add,
-and click the `Install` button on the right side of the table. When the extension is installed,
-you'll see it jump to the top of the list, and a status message will appear at the bottom of the screen.
-
-Extensions are lightweight -- install as many as you want! When an update is available for an
-extension, you'll see an `Update` button next to the `Remove` option.
-
-![extensions page screenshot](/img/screenshot_extensions.png)
-
-:::tip
-Some extensions have their own settings. See the **[Extension Settings](/docs/using-extensions/extension-settings)** page for more info.
-:::
-
-## Adding a Series
-
-To add a series to your library, go to the `Add Series` tab on the left. Select the extension
-you want to use from the dropdown at the top, and search for the series you want to add.
-
-Clicking a cover image will display a pop-up modal with the series info retrieved from the extension. 
-For filesystem imports, you can (optionally) fill out this information yourself. Click the
-`Add Series` button at the bottom to add it to your library.
-
-For more info about filesystem imports, see the **[Adding Local Series](/docs/adding-manga/adding-local-series)** page.
-
----
-Houdoku is in active development. Please report bugs or request features [on GitHub](https://github.com/xgi/houdoku/issues).
+<GettingStartedRow />
+```
---
+++ src/components/GettingStarted.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import Link from "@docusaurus/Link";
+import clsx from "clsx";
+
+const Topics = [
+  {
+    name: "Add Manga",
+    links: [
+      {
+        title: "Add from websites",
+        url: "/docs/adding-manga/adding-from-websites",
+      },
+      {
+        title: "Add from filesystem",
+        url: "/docs/adding-manga/adding-local-series",
+      },
+    ],
+    description:
+      "Read manga from supported aggregator/scanlator websites, or import from your filesystem.",
+  },
+  {
+    name: "Configuration",
+    links: [{ title: "Learn more", url: "/docs/configuration" }],
+    description:
+      "Customize your library layout, change your reader keybindings, sync with your AniList account, and more.",
+  },
+  {
+    name: "Offline Downloading",
+    links: [{ title: "Learn more", url: "/docs/offline-downloading" }],
+    description: "Download manga to read when you're offline.",
+  },
+  {
+    name: "GitHub",
+    links: [
+      { title: "Go to repository", url: "https://github.com/xgi/houdoku" },
+    ],
+    description: "Report bugs, request features, or contribute to Houdoku.",
+  },
+];
+
+function GettingStartedCard({ name, links, description }) {
+  return (
+    <div className="col col--6 margin-bottom--lg">
+      <div className={clsx("card")}>
+        <div className="card__body">
+          <h3>{name}</h3>
+          <p>{description}</p>
+        </div>
+        <div className="card__footer">
+          <div className="button-group button-group--block margin-bottom--sm">
+            {links.map((link) => (
+              <Link className="button button--secondary" to={link.url}>
+                {link.title}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function GettingStartedRow() {
+  return (
+    <div className="row">
+      {Topics.map((topic) => (
+        <GettingStartedCard key={topic.name} {...topic} />
+      ))}
+    </div>
+  );
+}
