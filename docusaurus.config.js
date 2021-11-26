ada6ac6ef88e9ba469911723f6d1be6065746a70
Jake Robertson <jake@faltro.com>
2021-11-25 17:00:31 -0800

    remove footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,6 +1,3 @@
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
-
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
   title: "Houdoku",
@@ -41,55 +38,6 @@ module.exports = {
         },
       ],
     },
-    footer: {
-      style: "dark",
-      // links: [
-      //   {
-      //     title: 'Docs',
-      //     items: [
-      //       {
-      //         label: 'Tutorial',
-      //         to: '/docs/intro',
-      //       },
-      //     ],
-      //   },
-      //   {
-      //     title: 'Community',
-      //     items: [
-      //       {
-      //         label: 'Stack Overflow',
-      //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-      //       },
-      //       {
-      //         label: 'Discord',
-      //         href: 'https://discordapp.com/invite/docusaurus',
-      //       },
-      //       {
-      //         label: 'Twitter',
-      //         href: 'https://twitter.com/docusaurus',
-      //       },
-      //     ],
-      //   },
-      //   {
-      //     title: 'More',
-      //     items: [
-      //       {
-      //         label: 'Blog',
-      //         to: '/blog',
-      //       },
-      //       {
-      //         label: 'GitHub',
-      //         href: 'https://github.com/facebook/docusaurus',
-      //       },
-      //     ],
-      //   },
-      // ],
-      copyright: `Copyright © 2018 - 2021 <a href="https://faltro.com" target="_blank">Jake Robertson</a> // <a href="https://github.com/xgi/houdoku-website/blob/master/LICENSE.txt" target="_blank">MIT License</a>`,
-    },
-    // prism: {
-    //   theme: lightCodeTheme,
-    //   darkTheme: darkCodeTheme,
-    // },
   },
   presets: [
     [
